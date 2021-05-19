4bc85f2f1327b865508f4bc8cd3ecd519320e9c9
Origho-precious <origho9@gmail.com>
2021-05-19 22:55:55 +0100

    feat: adding app models

---
+++ backend/models/orderModel.js
@@ -0,0 +1,77 @@
+import { Mongoose, Schema } from "mongoose";
+
+const orderSchema = Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: "User",
+		},
+		orderItems: [
+			{
+				name: { type: String, required: true },
+				qty: { type: Number, required: true },
+				image: { type: String, required: true },
+				price: { type: Number, required: true },
+				product: {
+					type: Schema.Types.ObjectId,
+					required: true,
+					ref: "Product",
+				},
+			},
+		],
+		shippingAddres: {
+			address: { type: String, required: true },
+			city: { type: String, required: true },
+			postalCode: { type: String, required: true },
+			country: { type: String, required: true },
+		},
+		paymentMethod: {
+			type: String,
+			required: true,
+		},
+		paymentResult: {
+			id: { type: String },
+			status: { type: String },
+			update_time: { type: String },
+			email: { type: String },
+		},
+		taxPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		taxPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		shippingPice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		totalPrice: {
+			type: Number,
+			required: true,
+			default: 0.0,
+		},
+		isPaid: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		paidAt: Date,
+		isDelivered: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		deliveredAt: Date,
+	},
+	{ timestamps: true }
+);
+
+const Order = Mongoose.model("Order", orderSchema);
+
+export default Order;
