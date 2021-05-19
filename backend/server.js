85a064eb6d98eb4c62ce2a74cc8501df89ed570e
Origho-precious <origho9@gmail.com>
2021-05-19 18:14:29 +0100

    feat: using env variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
@@ -11,8 +14,9 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/products/:id", (req, res) => {
 	const product = products.find((product) => product._id === req.params.id);
-	
-  res.json(product);
+	res.json(product);
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} mode on ${PORT}`));
