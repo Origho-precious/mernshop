78d62094c6372d80a6956cd75e9a5171dbaba74c
Origho-precious <origho9@gmail.com>
2021-05-20 20:25:54 +0100

    feat: seperating products routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/product.js";
 
 dotenv.config();
 
@@ -11,15 +11,7 @@ connectDB();
 const app = express();
 
 app.get("/", (req, res) => {});
-
-app.get("/api/products", (req, res) => {
-	res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((product) => product._id === req.params.id);
-	res.json(product);
-});
+app.use("/api/product", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -29,3 +21,4 @@ app.listen(
 		`Server running ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
 	)
 );
+
