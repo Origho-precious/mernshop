eb2dd966fca8c26500c718b8fc2c3d4706d67a49
Origho-precious <origho9@gmail.com>
2021-05-21 03:04:49 +0100

    feat: using Loader and Message component

--- frontend/src/pages/HomeScreen/index.jsx
+++ frontend/src/pages/HomeScreen/index.jsx
@@ -3,10 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
 import Product from "../../components/Product/Product";
 import { getProductList } from "../../store/slices/products.slice";
+import Loader from "../../components/Loader/Loader";
+import Message from "../../components/Message/Message";
 
 const HomePage = () => {
 	const dispatch = useDispatch();
-	const { products } = useSelector((state) => state.productList);
+	const { products, loading, error } = useSelector(
+		(state) => state.productList
+	);
 
 	useEffect(() => {
 		dispatch(getProductList());
@@ -15,13 +19,19 @@ const HomePage = () => {
 	return (
 		<>
 			<h1 className="mt-3 mb-4">Latest Products</h1>
-			<Row>
-				{products?.map((product) => (
-					<Col key={product?._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<Row>
+					{products?.map((product) => (
+						<Col key={product?._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	);
 };
