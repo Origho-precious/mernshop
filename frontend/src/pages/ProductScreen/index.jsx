8dd69fbc8a7b365e2baec7f9f8b85a75785e3ce9
Origho-precious <origho9@gmail.com>
2021-05-21 03:54:08 +0100

    refactor: refactoring product details page

--- frontend/src/pages/ProductScreen/index.jsx
+++ frontend/src/pages/ProductScreen/index.jsx
@@ -19,71 +19,67 @@ const ProductScreen = ({ match }) => {
 
 	return (
 		<>
+			<Link to="/" className="btn btn-light my-3">
+				Go back
+			</Link>
 			{loading ? (
 				<Loader />
 			) : error ? (
 				<Message variant="danger">{error}</Message>
 			) : (
-				<>
-					<Link to="/" className="btn btn-light my-3">
-						Go back
-					</Link>
-					<Row>
-						<Col md={6}>
-							<Image fluid src={product?.image} alt={product?.name} />
-						</Col>
-						<Col md={3}>
+				<Row>
+					<Col md={6}>
+						<Image fluid src={product?.image} alt={product?.name} />
+					</Col>
+					<Col md={3}>
+						<ListGroup variant="flush">
+							<ListGroup.Item>
+								<h3 className="pb-2">{product?.name}</h3>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Rating
+									value={product?.rating}
+									text={`${product?.numReviews} reviews`}
+								/>
+							</ListGroup.Item>
+							<ListGroup.Item>Price: ${product?.price}</ListGroup.Item>
+							<ListGroup.Item>
+								Description: ${product?.description}
+							</ListGroup.Item>
+						</ListGroup>
+					</Col>
+					<Col md={3}>
+						<Card>
 							<ListGroup variant="flush">
 								<ListGroup.Item>
-									<h3 className="pb-2">{product?.name}</h3>
+									<Row>
+										<Col>Price:</Col>
+										<Col>
+											<strong>${product?.price}</strong>
+										</Col>
+									</Row>
 								</ListGroup.Item>
 								<ListGroup.Item>
-									<Rating
-										value={product?.rating}
-										text={`${product?.numReviews} reviews`}
-									/>
+									<Row>
+										<Col>Status:</Col>
+										<Col>
+											{product?.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+										</Col>
+									</Row>
 								</ListGroup.Item>
-								<ListGroup.Item>Price: ${product?.price}</ListGroup.Item>
 								<ListGroup.Item>
-									Description: ${product?.description}
+									<Button
+										disabled={product?.countInStock > 0}
+										type="button"
+										className="btn-block"
+									>
+										ADD TO CART
+									</Button>
 								</ListGroup.Item>
 							</ListGroup>
-						</Col>
-						<Col md={3}>
-							<Card>
-								<ListGroup variant="flush">
-									<ListGroup.Item>
-										<Row>
-											<Col>Price:</Col>
-											<Col>
-												<strong>${product?.price}</strong>
-											</Col>
-										</Row>
-									</ListGroup.Item>
-									<ListGroup.Item>
-										<Row>
-											<Col>Status:</Col>
-											<Col>
-												{product?.countInStock > 0
-													? "In Stock"
-													: "Out Of Stock"}
-											</Col>
-										</Row>
-									</ListGroup.Item>
-									<ListGroup.Item>
-										<Button
-											disabled={product?.countInStock > 0}
-											type="button"
-											className="btn-block"
-										>
-											ADD TO CART
-										</Button>
-									</ListGroup.Item>
-								</ListGroup>
-							</Card>
-						</Col>
-					</Row>
-				</>
+						</Card>
+					</Col>
+				</Row>
 			)}
 		</>
 	);
