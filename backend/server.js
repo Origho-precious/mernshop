35b073329b00adb9d6ea2cd8c501ec3a036ef8b4
Origho-precious <origho9@gmail.com>
2021-05-22 20:40:55 +0100

    feat: refactoring product routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import productRoutes from "./routes/product.js";
-import {notFound, errorHandler} from './middlewares/error.middleware.js';
+import productRoutes from "./routes/product.route.js";
+import { notFound, errorHandler } from "./middlewares/error.middleware.js";
 
 dotenv.config();
 
@@ -11,13 +11,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 app.use("/api/products", productRoutes);
 
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
