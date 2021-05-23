e3c6d44fc4cd7f19c104f2b8c90853cc68d437b5
Origho-precious <origho9@gmail.com>
2021-05-23 01:22:13 +0100

    feat: adding user registration endpoint

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { authUser, getUserProfile } from "../controllers/user.controller.js";
-import { protect } from '../middlewares/auth.midddleware.js';
+import {
+	authUser,
+	getUserProfile,
+	registerUser,
+} from "../controllers/user.controller.js";
+import { protect } from "../middlewares/auth.midddleware.js";
 
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
