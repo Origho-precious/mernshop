3740005d9bf8d3a39e06172921033b7989e13213
Origho-precious <origho9@gmail.com>
2021-05-25 14:41:54 +0100

    feat: adding update profile endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -26,7 +26,7 @@ export const authUser = asyncHandler(async (req, res) => {
 
 // @desc Get user profile
 // @route GET /api/users/profile
-// @access Public
+// @access Private
 export const getUserProfile = asyncHandler(async (req, res) => {
 	const user = await User.findById(req.user._id).select(["-password", "-__v"]);
 
@@ -64,5 +64,34 @@ export const registerUser = asyncHandler(async (req, res) => {
 	} else {
 		res.status(400);
 		throw new Error("Invalid user data");
+	}
+});
+
+// @desc Update user profile
+// @route PATCH /api/users/profile/:id
+// @access Public
+export const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.status(200).json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
 	}
 });
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,6 +3,7 @@ import {
 	authUser,
 	getUserProfile,
 	registerUser,
+	updateUserProfile,
 } from "../controllers/user.controller.js";
 import { protect } from "../middlewares/auth.midddleware.js";
 
@@ -10,6 +11,9 @@ const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+	.route("/profile")
+	.get(protect, getUserProfile)
+	.patch(protect, updateUserProfile);
 
 export default router;
