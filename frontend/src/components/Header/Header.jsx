29fbb6e23ad087eb236e8096917b3317ce1f115c
Origho-precious <origho9@gmail.com>
2021-06-04 14:47:25 +0100

    feat: adding admin dropdown to header comp

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -38,6 +38,22 @@ const Header = () => {
 									</Nav.Link>
 								</LinkContainer>
 							)}
+							{userInfo?.isAdmin && (
+								<NavDropdown title='Admin' id="adminmenu">
+									<LinkContainer to="/admin/users">
+										<NavDropdown.Item>Users</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to="/admin/productlist">
+										<NavDropdown.Item>Products</NavDropdown.Item>
+									</LinkContainer>
+									<LinkContainer to="/admin/orderlist">
+										<NavDropdown.Item>Orders</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={() => dispatch(logout())}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
