2b9c29793a054672c954f402f6470b3d86d1d590
Origho-precious <origho9@gmail.com>
2021-06-11 20:03:29 +0100

    deployment preparaation

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,6 @@ app.use(express.json());
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-	res.send("API is running...");
-});
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -32,6 +29,18 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 app.use("/api/upload", uploadRoute);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running...");
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
