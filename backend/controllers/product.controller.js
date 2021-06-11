f677d86f1fd5198a6489f7b16464228d874f5082
Origho-precious <origho9@gmail.com>
2021-06-11 14:49:27 +0100

    code fix

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,7 +5,7 @@ import Product from "../models/product.model.js";
 // @route GET /api/products
 // @access Public
 export const getProducts = asyncHandler(async (req, res) => {
-	const pageSize = 2;
+	const pageSize = 10;
 	const page = Number(req.query.pageNumber) || 1;
 
 	const searchFilter = req.query.keyword
